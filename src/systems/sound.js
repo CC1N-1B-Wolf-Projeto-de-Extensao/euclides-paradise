6a528177285f49da21cab3140d646c9c10cac199
Pam-Devil <sudopamdevil@gmail.com>
2026-06-09 16:25:17 -0300

    fixed sound effects

--- src/systems/sound.js
+++ src/systems/sound.js
@@ -1,11 +1,11 @@
 export const sounds = {
-    match: "../../assets/sound/sound_effect.mp3",
-    explosion: "../../assets/sound/explosion.mp3"
+    match: new URL("../../assets/sound/sound_effect.mp3", import.meta.url),
+    explosion: new URL("../../assets/sound/explosion.mp3", import.meta.url)
 }
 
 export function play(name) {
 
-    const sound = new Audio(sounds[name]);
+    const sound = new Audio(sounds[name])
 
     sound.volume = 0.4;
 
