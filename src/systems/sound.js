990b1879292989715009e350e201c8b47a4bc4f9
Pam-Devil <sudopamdevil@gmail.com>
2026-06-09 16:41:54 -0300

    fixed sound effects

--- src/systems/sound.js
+++ src/systems/sound.js
@@ -1,13 +1,20 @@
 export const sounds = {
-    match: new URL("../../assets/sound/sound_effect.mp3", import.meta.url),
-    explosion: new URL("../../assets/sound/explosion.mp3", import.meta.url)
+    match: "sound_effect",
+    explosion: "explosion"
 }
 
 export function play(name) {
 
-    const base = window.location.pathname.split("/")[1];
+    const base = window.location.origin.split("/")[1];
 
-    const sound = new Audio(`/${base}/assets/sound/${name}.mp3`)
+    const BASE_PATH =
+    window.location.hostname === "127.0.0.1"
+    || window.location.hostname === "localhost"
+        ? ""
+        : "/euclides-paradise";
+
+    const sound = new Audio(`${BASE_PATH}/assets/sound/${sounds[name]}.mp3`)
+    console.log(sound.src)
 
     sound.volume = 0.4;
 
